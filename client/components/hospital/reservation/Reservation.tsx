668867efc30d6b0e1dc1d8ec85635af80a983bb7
chspower1 <chspower1@gmail.com>
2023-01-18 13:44:50 +0900

    feat(Reservation) : Reservation 컴포넌트 틀 잡기

---
+++ client/components/hospital/reservation/Reservation.tsx
@@ -0,0 +1,11 @@
+import { ContentBox } from "@/styles/Hospital";
+import HospitalList from "../HospitalList";
+
+const Reservation = () => {
+  return (
+    <ContentBox>
+      <HospitalList />
+    </ContentBox>
+  );
+};
+export default Reservation;
