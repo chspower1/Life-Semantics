d429dc945cd2e3de5e4acee70160d4a3c7650135
chspower1 <chspower1@gmail.com>
2023-01-20 08:33:04 +0900

    style(hospital) : hospital 공통 레이아웃 초안 작성

--- client/styles/Hospital.ts
+++ client/styles/Hospital.ts
@@ -1,3 +1,25 @@
 import styled from "styled-components";
-
-export const ContentBox = styled.div``;
+import { FlexBox } from "./Common";
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  width: 33%;
+  height: 100%;
+`;
+export const ContentTitle = styled(FlexBox)`
+  width: 100%;
+  height: 50px;
+`;
+export const ContentBox = styled(FlexBox)`
+  flex-direction: column;
+  width: 100%;
+  height: 80%;
+  background-color: #999999;
+  gap: 10px;
+`;
+export const Item = styled(FlexBox)`
+  width: 90%;
+  height: 100px;
+  background-color: white; ;
+`;
