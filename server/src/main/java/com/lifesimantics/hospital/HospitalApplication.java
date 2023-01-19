47817e474285b31148e7f1a248adb4a131671b52
chspower1 <chspower1@gmail.com>
2023-01-20 07:00:37 +0900

    feat(backend) : cors 적용

--- server/src/main/java/com/lifesimantics/hospital/HospitalApplication.java
+++ server/src/main/java/com/lifesimantics/hospital/HospitalApplication.java
@@ -2,6 +2,9 @@ package com.lifesimantics.hospital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HospitalApplication {
@@ -10,4 +13,13 @@ public class HospitalApplication {
 		SpringApplication.run(HospitalApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
