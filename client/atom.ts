61542e1b34d872f0da552297f066a64bd53ea432
chspower1 <chspower1@gmail.com>
2023-01-24 02:39:45 +0900

    feat(frontend) : user 전역변수 선언

--- client/atom.ts
+++ client/atom.ts
@@ -2,10 +2,10 @@ import { atom } from "recoil";
 import { Hospital } from "./types/hospital";
 import { User } from "./types/user";
 
-export const userState=atom<User | null>({
-  key:"user",
-  default:null;
-})
+export const userState = atom<User | null>({
+  key: "user",
+  default: null,
+});
 
 export const selectedHospitalState = atom<Hospital | null>({
   key: "selectedHospital",
