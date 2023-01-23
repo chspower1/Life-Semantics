766f36457c273589a9324fd3d6e6ba7290ac496c
chspower1 <chspower1@gmail.com>
2023-01-24 02:53:42 +0900

    feat(frontend) : login 로직 구현, token localStorage 저장

--- client/pages/auth/login.tsx
+++ client/pages/auth/login.tsx
@@ -3,30 +3,45 @@ import { Col as Form, Wrapper } from "@/styles/Common";
 import { ErrorMessage, InputBox, SubmitButton, Subtitle, Title } from "@/styles/FormStyle";
 import customApi from "@/utils/customApi";
 import { useMutation, useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useForm } from "react-hook-form";
 
 interface LoginForm {
   accountId: string;
   password: string;
 }
-
+interface ResponseLogin {
+  user: {
+    id: number;
+    accountId: string;
+    name: string;
+  };
+  token: string;
+}
 const LoginPage = () => {
+  const router = useRouter();
   const { postApi: loginApi } = customApi<LoginForm>("http://localhost:8080/auth/login");
-  const { postApi: checkApi } = customApi("http://localhost:8080/auth/check");
-  const { mutateAsync: loginMutate } = useMutation(["login"], loginApi);
-  const { mutateAsync: checkMutate } = useMutation(["check"], loginApi);
+  const { mutate: loginMutate } = useMutation<ResponseLogin, AxiosError, LoginForm>(
+    ["login"],
+    loginApi,
+    {
+      onSuccess(data) {
+        if (data) {
+          localStorage.setItem("token", data.token);
+          router.push("/hospital");
+        } else alert("사용자 정보가 옳지 않습니다!");
+      },
+    }
+  );
   const {
     register,
     formState: { errors },
     handleSubmit,
   } = useForm<LoginForm>();
   const onValid = async ({ accountId, password }: LoginForm) => {
-    const token = await loginMutate({ accountId, password });
-    console.log(token);
-    localStorage.setItem("token", token);
-
-    console.log({ accountId, password });
+    loginMutate({ accountId, password });
   };
 
   return (
