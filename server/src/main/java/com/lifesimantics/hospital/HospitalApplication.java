db0d215e5d48a4a5b8341d1588dc8ec7d8b447df
chspower1 <chspower1@gmail.com>
2023-01-24 06:02:32 +0900

    feat(backend): CORS PUT,DELETE 추가

--- server/src/main/java/com/lifesimantics/hospital/HospitalApplication.java
+++ server/src/main/java/com/lifesimantics/hospital/HospitalApplication.java
@@ -18,7 +18,10 @@ public class HospitalApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry
+				.addMapping("/**")
+				.allowedMethods("GET", "POST","PUT","DELETE")
+				.allowedOrigins("http://localhost:3000");
 			}
 		};
 	}
